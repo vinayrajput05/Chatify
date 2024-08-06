a6571df6079cb7d305951e261b92bd50bf1445ce
codewithvinaysingh <codewithvinaysingh@gmail.com>
2024-08-06 11:42:57 +0000

    Initial Commit with Shadcn Ui and React Router Dom

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter,Route, Routes, Navigate } from 'react-router-dom'
+import Auth from './pages/auth'
+import Profile from './pages/profile'
+import Chat from './pages/chat'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='*' element={<Navigate to='/auth'/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
